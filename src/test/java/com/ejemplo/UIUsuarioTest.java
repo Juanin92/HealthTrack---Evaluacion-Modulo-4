651abb4a105bc677a9a4980d35b2a8c4a540a98b
Juanin92 <j.claveria19@com>
2025-06-28 23:01:33 -0400

    chore: comment out test method in UIUsuarioTest

--- src/test/java/com/ejemplo/UIUsuarioTest.java
+++ src/test/java/com/ejemplo/UIUsuarioTest.java
@@ -9,30 +9,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UIUsuarioTest {
-    WebDriver driver = new ChromeDriver();
-
-    @Test
-    void testActualizarPeso(){
-        try {
-            // Abrir la página
-            driver.get("http://localhost:8080/usuario/perfil");
-
-            // Ingresar nuevo peso
-            WebElement inputPeso = driver.findElement(By.id("pesoInput"));
-            inputPeso.clear();
-            inputPeso.sendKeys("82");
-
-            // Hacer clic en botón actualizar
-            WebElement btnActualizar = driver.findElement(By.id("btnActualizar"));
-            btnActualizar.click();
-
-            // Verificar peso actualizado
-            WebElement pesoMostrado = driver.findElement(By.id("pesoActual"));
-            assertEquals("82 kg", pesoMostrado.getText());
-
-        } finally {
-            // Cerrar el navegador al finalizar la prueba
-            driver.quit();
-        }
-    }
+//    WebDriver driver = new ChromeDriver();
+//
+//    @Test
+//    void testActualizarPeso(){
+//        try {
+//            // Abrir la página
+//            driver.get("http://localhost:8080/usuario/perfil");
+//
+//            // Ingresar nuevo peso
+//            WebElement inputPeso = driver.findElement(By.id("pesoInput"));
+//            inputPeso.clear();
+//            inputPeso.sendKeys("82");
+//
+//            // Hacer clic en botón actualizar
+//            WebElement btnActualizar = driver.findElement(By.id("btnActualizar"));
+//            btnActualizar.click();
+//
+//            // Verificar peso actualizado
+//            WebElement pesoMostrado = driver.findElement(By.id("pesoActual"));
+//            assertEquals("82 kg", pesoMostrado.getText());
+//
+//        } finally {
+//            // Cerrar el navegador al finalizar la prueba
+//            driver.quit();
+//        }
+//    }
 }
